bda81a8c647bfe4e6079066e5ddc28145fa41c20
Carlosecopm <carlosluizecomp.uefs@gmail.com>
2021-10-09 02:16:32 -0300

    Add desafio numerico

---
+++ desafioNumerico/src/com/company/Problem2.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Problem2 {
+    public static void main(String[] args){
+
+        Scanner ent = new Scanner(System.in);
+        int num, fat = 1;
+        int cont = 1;
+
+        do{
+            //System.out.println("Digite um nº");
+            num = ent.nextInt();
+
+            for(int i = 1;i <= num; i++){
+                fat = fat * i;
+            }
+
+            System.out.println(fat);
+            cont++;
+
+        }while(cont < 2);
+    }
+}
