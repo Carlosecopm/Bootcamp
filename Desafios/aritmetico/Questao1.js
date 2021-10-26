192f25ac637bce5a2773650e613c124f6755fb1f
Carlosecopm <carlosluizecomp.uefs@gmail.com>
2021-10-25 22:36:41 -0300

    Add microServico

---
+++ Desafios/aritmetico/Questao1.js
@@ -0,0 +1,26 @@
+
+ //const isSum = lines.shift() === "S";
+ 
+ const xL = 12,
+   yL = 12;
+ 
+ //let sum = 0.0,
+  // av = 0.0;
+   //var cont = 0;
+ 
+ for (let i = xL; i >= 0; xL -- ) {
+   for (let y = yL; y >= 0; yL) {
+       cont += cont + 1
+     //const i = Number(lines.shift());
+     //if (                    ) {
+       //sum += i;
+       //++av;
+     //}
+   }
+ }
+ console.log("cont", cont);
+ //console.log((sum / (isSum ? 1 : av)).toFixed("1"));
+/**
+ * 
+ */
+//console.log("mostrando console");
