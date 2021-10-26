bcf68556521c2c114db7e5f8893c37f3411e2b61
Carlosecopm <carlosluizecomp.uefs@gmail.com>
2021-10-25 22:34:54 -0300

    Inicio JavaScript

---
+++ web/bootstrap/javaScript/main.js
@@ -0,0 +1,12 @@
+//var nome = "Carlos Silva";
+//alert("Bem vindo" + nome);
+/*var idade = prompt("Qual sua idade ?");
+if (idade >= 18) {
+    alert("Maior de idade");
+} else {
+    alert("Menor de idade");
+}
+*/
+
+var data = new Date()
+alert(data)
